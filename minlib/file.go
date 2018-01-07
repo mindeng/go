5ebe42ff9dbbf6ba9a0fe006a906593ec779c373
Min Deng <minotes.net@gmail.com>
2018-01-07 12:49:25 +0800

    Add minlib.EqualFile()

--- minlib/file.go
+++ minlib/file.go
@@ -1,9 +1,11 @@
 package minlib
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -63,4 +65,58 @@ func CopyFile(dst, src string) error {
 	}
 
 	return os.Rename(tmp.Name(), dst)
+}
+
+const chunkSize = 64 * 1024
+
+func EqualFile(file1, file2 string) bool {
+	fi1, err := os.Stat(file1)
+	if os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+	fi2, err := os.Stat(file2)
+	if os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	if fi1.Size() != fi2.Size() {
+		return false
+	}
+
+	f1, err := os.Open(file1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+
+	b1 := make([]byte, chunkSize)
+	b2 := make([]byte, chunkSize)
+	for {
+		_, err1 := io.ReadFull(f1, b1)
+		_, err2 := io.ReadFull(f2, b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false
+			} else if err1 == io.ErrUnexpectedEOF && err2 == io.ErrUnexpectedEOF {
+				return bytes.Equal(b1, b2)
+			} else if err1 == io.ErrUnexpectedEOF || err2 == io.ErrUnexpectedEOF {
+				return false
+			} else {
+				log.Fatal(err1, err2)
+			}
+		}
+
+		if !bytes.Equal(b1, b2) {
+			return false
+		}
+	}
 }
