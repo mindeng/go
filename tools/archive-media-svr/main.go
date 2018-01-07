943693357e0423de19506c32b8e64d553ccc72ff
Min Deng <minotes.net@gmail.com>
2018-01-08 01:38:52 +0800

    archive-media-svr: concurrent verify checksum

--- tools/archive-media-svr/main.go
+++ tools/archive-media-svr/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"bufio"
-	"crypto/md5"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	"github.com/mindeng/go/minlib"
 )
 
 var host = flag.String("host", "", "host")
@@ -39,8 +40,55 @@ func main() {
 		go handleRequest(conn)
 	}
 }
+
+type VerifyJob struct {
+	path     string
+	checksum string
+	result   bool
+}
+
+func handleVerifyJob(jobs <-chan VerifyJob, results chan<- VerifyJob, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		dst := filepath.Join(flag.Args()[0], job.path)
+		myChecksum, err := minlib.FileChecksum(dst)
+		if err != nil {
+			log.Fatal(err)
+		}
+		job.result = myChecksum == job.checksum
+		results <- job
+	}
+}
+
+func handleSendResponse(conn net.Conn, jobs <-chan VerifyJob, wg *sync.WaitGroup) {
+	for job := range jobs {
+		ok := "miss"
+		if job.result {
+			ok = "ok"
+		}
+		result := fmt.Sprintf("%s\t%v\n", job.path, ok)
+		fmt.Print(result)
+		conn.Write([]byte(result))
+	}
+	wg.Done()
+}
+
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
+
+	jobs := make(chan VerifyJob, 100)
+	results := make(chan VerifyJob, 100)
+
+	const concurrentNum = 4
+	var wg sync.WaitGroup
+	wg.Add(concurrentNum)
+	for i := 0; i < concurrentNum; i++ {
+		go handleVerifyJob(jobs, results, &wg)
+	}
+
+	var wgSend sync.WaitGroup
+	wgSend.Add(1)
+	go handleSendResponse(conn, results, &wgSend)
 
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
@@ -54,52 +102,30 @@ func handleRequest(conn net.Conn) {
 		}
 
 		if params[0] == "done" && params[1] == "done" {
-			// done
+			// all jobs have been received
+			close(jobs)
+
+			// wait for all jobs done
+			wg.Wait()
+
+			// all results have been sent
+			close(results)
+
+			// wait for all results sent
+			wgSend.Wait()
+
+			// send done signal
 			result := fmt.Sprintf("done\tdone\n")
-			fmt.Println("done")
+			fmt.Print(result)
 			conn.Write([]byte(result))
-			continue
+			break
 		}
 
 		path, checksum := params[0], params[1]
-
-		dst := filepath.Join(flag.Args()[0], path)
-
-		myChecksum, err := fileChecksum(dst)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		ok := "miss"
-		if checksum == myChecksum {
-			ok = "ok"
-		}
-		result := fmt.Sprintf("%s\t%v\n", path, ok)
-		fmt.Println(line, " ", ok)
-		conn.Write([]byte(result))
+		jobs <- VerifyJob{path, checksum, false}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	// for {
-	// 	io.Copy(conn, conn)
-	// }
-}
-
-func fileChecksum(path string) (string, error) {
-	f, err := os.Open(path)
-	defer f.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return fmt.Sprintf("%x", md5.Sum(data)), nil
 }
