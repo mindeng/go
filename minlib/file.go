ea59e6e2d98cfb10e0c8a1401e0289f53372b550
Min Deng <minotes.net@gmail.com>
2018-02-04 22:38:38 +0800

    Add CopyFileFromReader

--- minlib/file.go
+++ minlib/file.go
@@ -68,6 +68,38 @@ func CopyFile(dst, src string) error {
 	return os.Rename(tmp.Name(), dst)
 }
 
+// CopyFile copies the contents from src to dst atomically.
+// If dst does not exist, CopyFile creates it and preserve the modification time.
+// If the copy fails, CopyFile aborts and dst is preserved.
+func CopyFileFromReader(dst string, src io.Reader, modTime time.Time) error {
+	f, err := os.OpenFile(dst, os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		return err
+	}
+	f.Close()
+
+	in := src
+
+	// tmp, err := ioutil.TempFile("", "")
+	tmp, err := ioutil.TempFile(filepath.Dir(dst), "_tmp_")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmp.Name())
+
+	_, err = io.Copy(tmp, in)
+	if err = tmp.Close(); err != nil {
+		return err
+	}
+
+	if err = os.Chtimes(tmp.Name(), time.Now(), modTime); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+
+	return os.Rename(tmp.Name(), dst)
+}
+
 const chunkSize = 64 * 1024
 
 func EqualFile(file1, file2 string) bool {
